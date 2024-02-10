52f6b2203fcd504e8048c2cb9ec6865869912a46
The_Unknown <128997233+Unknown-strange@users.noreply.github.com>
2024-02-10 17:11:26 +0000

    Add files via upload

---
+++ for_today.py
@@ -0,0 +1,15 @@
+wedding_lists = ["kofi@gmail.com","AMA@gmail.com","Prince@gmail.com"]
+print(wedding_lists)
+
+wedding_lists.sort()
+
+wedding_lists.append("edwin@gmail.com")
+wedding_lists.remove("kofi@gmail.com")
+lengh_of_list = len(wedding_lists)
+new_wedding_list = []
+print(lengh_of_list)
+for wedding_list in wedding_lists:
+    wedding_list =str(wedding_list)
+    wedding_list = wedding_list.lower()
+    new_wedding_list = wedding_list
+    print(new_wedding_list)
